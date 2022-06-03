15dc6173ed927cc6363118459fd600739559d7a3
AlexandrKaminskiy <sasha.pinsk2003@gmail.com>
2022-06-04 02:45:46 +0300

    added some windows and controllers for them

---
+++ src/main/java/com/example/coursework/controller/ConnectGameController.java
@@ -0,0 +1,20 @@
+package com.example.coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ConnectGameController {
+
+    @FXML
+    private Button connectButton;
+
+    @FXML
+    private TextField ipAddress;
+
+    @FXML
+    void connectGame(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/com/example/coursework/controller/HostGameController.java
@@ -0,0 +1,20 @@
+package com.example.coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HostGameController {
+
+    @FXML
+    private Button createGameButton;
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    void createGame(ActionEvent event) {
+
+    }
+}
