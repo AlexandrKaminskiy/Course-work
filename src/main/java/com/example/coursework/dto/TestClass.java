bb868d196808a495e5fb1c397a4c2153e624b294
AlexandrKaminskiy <sasha.pinsk2003@gmail.com>
2022-06-06 10:40:38 +0300

    added normal bullet collisions

--- src/main/java/com/example/coursework/dto/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursework.dto;
-
-import java.io.Serializable;
-
-public class TestClass implements Serializable {
-    public int i;
-    public int j;
-    public PlayerDto playerDto = new PlayerDto();
-}
